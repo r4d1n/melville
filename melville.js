cee734996e3bac8329f12be75417fd3de534384e
Jared Radin <jared.radin@gmail.com>
2015-06-26 12:38:59 -0700

    recursive solution

--- melville.js
+++ melville.js
@@ -10,35 +10,80 @@ let target = 'whales.json';
 let regex = /(\w+)\s(whale)\s(\w+)/gim;
 let directory = './text/mobydick/';
 
-let rs = fs.createReadStream(directory+'moby-009.txt')
-//let ws = fs.createWriteStream(target, {flags: 'w'});
-let ws = fs.createWriteStream('test.txt', {flags: 'w'});
+//let rs = fs.createReadStream(directory+'moby-009.txt');
+let ws = fs.createWriteStream(target, {flags: 'w'});
+//let ws = fs.createWriteStream('test.txt', {flags: 'w'});
 
 //rs.pipe(ws);
-let counter = 0;
-let matches = [];
+// function checkFiles(files) {
+//   if (files.length > 0) {
+//     // do something
+//   } else {
+//
+//   }
+// }
 
-rs
-.pipe(split())
-.on('data', (chunk) => {
-  let obj = {};
+function getMatches(chunk, path) {
   let match = regex.exec(chunk);
   if (match) {
-    counter++;
-    //let chapter = /moby-(\d+)/.exec(el)[1];
-    let chapter = '009';
-    obj["match #" + counter] = {};
+    let chapter = /moby-(\d+)/.exec(path)[1];
+    //let chapter = '009';
     //console.log(counter);
     //let entry = { chapter, words : []}
-    let entry = {match: counter, chapter : chapter, words: []};
+    let entry = {chapter : chapter, words: []};
+    // let entry = {match: counter, chapter : chapter, words: []};
     let i = 1;
     while (i < 4) {
       entry.words.push(match[i]);
       i++;
     }
-    matches.push(entry);
+    return entry;
+  } else {
+    return null;
   }
-})
-.on('end', () => {
+}
+
+function dirPipe (dir) {
+  let files = util.makePaths(dir)
+  let matches = [];
+  let counter = 0;
+
+  return new Promise((resolve, reject) => {
+    function check() {
+      if (files.length === 0) {
+        // done!
+        resolve(matches);
+      } else {
+        let path = files.shift();
+        let rs = fs.createReadStream(path)
+
+        rs
+        .pipe(split())
+        .on('data', (chunk) => {
+          var entry = getMatches(chunk, path);
+          if(entry) {
+            entry.match = ++counter;
+            matches.push(entry);
+          }
+        })
+        .on('end', check);
+      }
+    }
+
+    if (_.isArray(files)) {
+        check();
+    } else {
+      reject(new Error('no result!'))
+      // resolve(matches);
+    }
+
+  });
+
+}
+
+dirPipe(directory).then((matches) => {
   ws.write(JSON.stringify(matches))
+  // console.log(matches);
+}).catch((err) => {
+  console.log(err);
 })
--- util.js
+++ util.js
@@ -1,25 +1,6 @@
-module.exports = {
-  // Promise wrapper from Kyle Simpson's book
-  // https://github.com/getify/You-Dont-Know-JS/blob/master/async%20%26%20performance/ch3.md
-  Promisify : function (fn) {
-    return function() {
-      var args = [].slice.call( arguments );
-      return new Promise (function(resolve,reject) {
-        fn.apply(
-          null,
-          args.concat( function(err,v){
-            if (err) {
-              reject( err );
-            }
-            else {
-              resolve( v );
-            }
-          } )
-        );
-      });
-    };
-  },
+let fs = require('fs');
 
+module.exports = {
   makePaths: function (dir) {
     let paths = fs.readdirSync(dir);
     return paths.filter((element) => {
@@ -28,5 +9,4 @@ module.exports = {
       return dir+element;
     })
   }
-
 }
